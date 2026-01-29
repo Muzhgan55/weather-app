12f7b5621775d3b3550202d6f4d6091f95256ef2
Muzhgan55 <muzhganrahimi8@gmail.com>
2026-01-29 12:00:38 +0430

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function displayTemperature(response) {
   windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
   timeElement.innerHTML = formatDate(date);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -55,5 +57,49 @@ function handleSearchSubmit(event) {
 
 let searchForm = document.querySelector("#search-bar");
 searchForm.addEventListener("submit", handleSearchSubmit);
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "bao59c44a03187a19379f00f52aadt46";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast">
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <div class="weather-forecast-icon">
+          <img src ="${day.condition.icon_url}"  />
+          </div>
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}°</strong>
+            </span>
+            <span class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</span>
+          </div>
+        </div>
+      </div>
+      `;
+    }
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
 
 searchCity("Kabul");
